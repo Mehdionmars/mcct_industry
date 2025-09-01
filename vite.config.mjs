d804601365fccd642015e210a820246e2df7c2e1
mehdionmars <mehdimoungids@gmail.com>
2025-09-01 23:41:14 +0100

    g

--- vite.config.mjs
+++ vite.config.mjs
@@ -11,6 +11,7 @@ export default defineConfig({
     outDir: "build",
     chunkSizeWarningLimit: 2000,
   },
+  base: "/mcct_industry",
   plugins: [tsconfigPaths(), react(), tagger()],
   server: {
     port: "4028",
