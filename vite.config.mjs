8d8db116ad7d41ee6d1ec9084737ab31be99cbde
mehdionmars <mehdimoungids@gmail.com>
2025-10-03 15:58:22 +0100

    cc

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,20 +3,16 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tagger from "@dhiwise/component-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
+  base: "/", // root deployment
+  plugins: [tsconfigPaths(), react(), tagger()],
   build: {
-  outDir: "dist",
+    outDir: "dist", // Vercel default for static deployments
     chunkSizeWarningLimit: 2000,
   },
-  base: "/",
-  plugins: [tsconfigPaths(), react(), tagger()],
   server: {
-    port: "4028",
+    port: 4028,
     host: "0.0.0.0",
     strictPort: true,
-    allowedHosts: []
-  }
+  },
 });
